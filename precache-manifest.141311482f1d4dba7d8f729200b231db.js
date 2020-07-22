3edc550d23741d962a0e54da29ad1b31cc9ba43f
Sundeep Chand <sundeep.chand2001@gmail.com>
2020-07-22 19:35:45 +0530

    Updates

---
+++ precache-manifest.141311482f1d4dba7d8f729200b231db.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "408959026daca6e9fbba05c539a669d6",
+    "url": "/Fun-Simon-Game/index.html"
+  },
+  {
+    "revision": "ea71e94fe21d0d6ac228",
+    "url": "/Fun-Simon-Game/static/css/main.a9b3b256.chunk.css"
+  },
+  {
+    "revision": "46f4708824e5b49cc7a6",
+    "url": "/Fun-Simon-Game/static/js/2.46850058.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Fun-Simon-Game/static/js/2.46850058.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fc244214864bc6938e7",
+    "url": "/Fun-Simon-Game/static/js/3.cb40b0b4.chunk.js"
+  },
+  {
+    "revision": "287e48aeac3564a8b2b5685a67fb9216",
+    "url": "/Fun-Simon-Game/static/js/3.cb40b0b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea71e94fe21d0d6ac228",
+    "url": "/Fun-Simon-Game/static/js/main.bb89d9e5.chunk.js"
+  },
+  {
+    "revision": "83071614f428dbe8fb4e",
+    "url": "/Fun-Simon-Game/static/js/runtime-main.140bdd36.js"
+  },
+  {
+    "revision": "1704d352f4b644504da23de8205a700e",
+    "url": "/Fun-Simon-Game/static/media/1.1704d352.mp3"
+  },
+  {
+    "revision": "afdc303f262af0e6d01b3fe00414d6ee",
+    "url": "/Fun-Simon-Game/static/media/2.afdc303f.mp3"
+  },
+  {
+    "revision": "edff8af408ef0532c598b65d43356286",
+    "url": "/Fun-Simon-Game/static/media/3.edff8af4.mp3"
+  },
+  {
+    "revision": "879d2195e7b826aaabb48ee6b6e2d15c",
+    "url": "/Fun-Simon-Game/static/media/4.879d2195.mp3"
+  },
+  {
+    "revision": "1ce68cdb7e2fa30192e736fbd53f2709",
+    "url": "/Fun-Simon-Game/static/media/GameOver.1ce68cdb.mp3"
+  },
+  {
+    "revision": "9de44e83947290f3dabca4220d816012",
+    "url": "/Fun-Simon-Game/static/media/logo.9de44e83.png"
+  },
+  {
+    "revision": "bab89d274b9f7211c39bc6ea94f4f828",
+    "url": "/Fun-Simon-Game/static/media/music.bab89d27.mp3"
+  }
+]);
